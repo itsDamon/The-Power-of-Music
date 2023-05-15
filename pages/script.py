8e219415c13d2b1ea565131aa31f1517f9c3648d
itsDamon <nick.parise20@gmail.com>
2023-05-15 13:53:23 +0200

    sistemo link

---
+++ pages/script.py
@@ -0,0 +1,10 @@
+import glob
+import os
+
+
+path =  os.path.dirname(os.path.realpath(__file__))
+
+for file in glob.glob("*.html"):
+    with open(file, "")
+    line = file.
+    while(line.strip() not "<nav>")
