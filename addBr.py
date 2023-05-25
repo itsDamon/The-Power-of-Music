9cf5a2fe5298da9e71f5175f4dd1488634ddef26
itsDamon <nick.parise20@gmail.com>
2023-05-25 10:58:25 +0200

    FINE

--- addBr.py
+++
@@ -1,5 +0,0 @@
-with open("addBr.txt", "r") as f:
-    with open("addedBr.txt", "w") as f2:
-        for line in f:
-            line = "<li>" +line.strip() + ("</li>")
-            f2.write(line + "\n")
